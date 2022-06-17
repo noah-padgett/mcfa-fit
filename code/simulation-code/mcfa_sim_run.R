5204fabbd012bc0e1073b382a92ffe240dd26e96
noah-padgett <40502329+noah-padgett@users.noreply.github.com>
2022-06-17 17:14:34 -0500

    updated to include simulation code page

--- code/simulation-code/mcfa_sim_run.R
+++ code/simulation-code/mcfa_sim_run.R
@@ -8,10 +8,26 @@
 # ============================= #
 library(MplusAutomation)
 createModels("mcfa_create_script_datagen.txt")
-createModels("createmodels_Spec_CL1_CL2.txt")
-createModels("createmodels_Spec_WL1_CL2.txt")
-createModels("createmodels_Spec_CL1_WL2.txt")
-createModels("createmodels_Spec_WL1_WL2.txt")
+createModels("createmodels_Spec_CL1_CL2_MLR.txt")
+createModels("createmodels_Spec_WL1_CL2_MLR.txt")
+createModels("createmodels_Spec_CL1_WL2_MLR.txt")
+createModels("createmodels_Spec_WL1_WL2_MLR.txt")
+
+createModels("createmodels_Spec_CL1_CL2_WLSMV.txt")
+createModels("createmodels_Spec_WL1_CL2_WLSMV.txt")
+createModels("createmodels_Spec_CL1_WL2_WLSMV.txt")
+createModels("createmodels_Spec_WL1_WL2_WLSMV.txt")
+
+createModels("createmodels_Spec_CL1_CL2_ULSMV.txt")
+createModels("createmodels_Spec_WL1_CL2_ULSMV.txt")
+createModels("createmodels_Spec_CL1_WL2_ULSMV.txt")
+createModels("createmodels_Spec_WL1_WL2_ULSMV.txt")
+
+# the WLSM was not analyzed
+# createModels("createmodels_Spec_CL1_CL2_WLSM.txt")
+# createModels("createmodels_Spec_WL1_CL2_WLSM.txt")
+# createModels("createmodels_Spec_CL1_WL2_WLSM.txt")
+# createModels("createmodels_Spec_WL1_WL2_WLSM.txt")
 
 runModels(recursive = T)
 
